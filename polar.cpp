40967733036c95b131b6cb59b7d6bdb9dc697acb
Geoffrey Boom <geoff.the.boom@gmail.com>
2014-10-01 16:49:30 -0400

    added some comments, added screenshot

--- polar.cpp
+++ polar.cpp
@@ -2,9 +2,16 @@
 #include <math.h>
 #include <SFML/Graphics.hpp>
 using namespace std;
+//true: print x and y 
 const bool printxy = true;
+//true: print i
 const bool printi = true;
+//true: puts images in a folder
+const bool outputimages = true;
+//time (in miliseconds) to wait to achieve 24 fps
 const int Mil_Per_Frame = 42;
+
+//--------------------functions--------------------//
 double f(double theta){
   return 50 + cos(100*theta);
 }
@@ -14,7 +21,13 @@ double f2(double theta){
 double f3(double theta){
   return (theta * theta);
 }
+
+
+//Number of times any function has been displayed
 static int numCompute = 0;
+
+//Function for displaying the polar function. 
+//Changes color after each run, loops through 3 colours
 void compute(double (* f)(double theta), sf::RenderWindow* window){
   double sx = (double)(window->getSize()).x;
   double sy = (double)(window->getSize()).y;
@@ -23,10 +36,15 @@ void compute(double (* f)(double theta), sf::RenderWindow* window){
     dot.setFillColor(sf::Color(100, 250, 50)); 
   if (numCompute == 1)
     dot.setFillColor(sf::Color(250, 50, 100));
-  numCompute = (numCompute + 1) %2;
+  if (numCompute == 2)
+    dot.setFillColor(sf::Color(50, 100, 250));
+  numCompute = (numCompute + 1) %3;
   double x = sx/2;
   double y = sy/2;
   double i = 0.0;
+
+  //While x and y are within the window bounds, and i is less than 1000
+  //i<1000 bound to prevent repeating functions from repeating forever.
   while((x < sx && x > 0.0 && y < sy && y > 0.0) && i < 1000.0){
     double theta = i / 10.0;
     double r = f(theta);
@@ -43,12 +61,11 @@ void compute(double (* f)(double theta), sf::RenderWindow* window){
     dot.setPosition(x,y);
     window->draw(dot);
     window->display();
-    i++;
   }
 }
 
 int main(){
-  sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(500, 500), "My window");
+  sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(500, 500), "Polar");
   window->clear();
   compute(f3,window);
   compute(f2,window);
