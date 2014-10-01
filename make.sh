9907a9156076d40135f806f792bdd9407e8d0072
Geoffrey Boom <geoff.the.boom@gmail.com>
2014-10-01 15:50:56 -0400

    Now works, instead of printing circles!

---
+++ make.sh
@@ -0,0 +1,2 @@
+g++ polar.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c polar.cpp -std=c++11
--- polar.cpp
+++ polar.cpp
@@ -2,9 +2,11 @@
 #include <math.h>
 #include <SFML/Graphics.hpp>
 using namespace std;
+const bool printxy = true;
+const bool printi = true;
 const int Mil_Per_Frame = 42;
 double f(double theta){
-  return cos(2*theta);
+  return 50 + cos(100*theta);
 }
 double f2(double theta){
   return 100 + sin(100*theta);
@@ -14,37 +16,39 @@ double f3(double theta){
 }
 static int numCompute = 0;
 void compute(double (* f)(double theta), sf::RenderWindow* window){
-  int sx = (window->getSize()).x;
-  int sy = (window->getSize()).y;
-  sf::CircleShape shape(1);
+  double sx = (double)(window->getSize()).x;
+  double sy = (double)(window->getSize()).y;
+  sf::CircleShape dot(1);
   if (numCompute == 0)
-    shape.setFillColor(sf::Color(100, 250, 50)); 
+    dot.setFillColor(sf::Color(100, 250, 50)); 
   if (numCompute == 1)
-    shape.setFillColor(sf::Color(250, 50, 100));
+    dot.setFillColor(sf::Color(250, 50, 100));
   numCompute = (numCompute + 1) %2;
   double x = sx/2;
   double y = sy/2;
-  int i = 0;
-  while(x < sx && x > 0 && y < sy && y > 0){
-  //for(int i = 0; i < 10000000; i++){
+  double i = 0.0;
+  while((x < sx && x > 0.0 && y < sy && y > 0.0) && i < 1000.0){
     double theta = i / 10.0;
     double r = f(theta);
-    double x = r * cos(theta);
-    double y = r * sin(theta);
-    printf("x: %f < %i y: %f < %i",x, sx,y, sy);
-    x = (x + 500);
-    y = (y + 500);
-    shape.setPosition(x,y);
-    window->draw(shape);
+    x = r * cos(theta);
+    y = r * sin(theta);
+    x = (x + sx/2);
+    y = (y + sy/2);
+    if (printxy){
+      printf("x: %f < %f y: %f < %f\n",x, sx,y, sy);
+    }
+    if (printi){
+      printf("i: %f\n", i);
+    }
+    dot.setPosition(x,y);
+    window->draw(dot);
     window->display();
     i++;
   }
 }
 
 int main(){
-  sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(1000, 1000), "My window");
-  //std::thread one(compute, f, window);
-  //std::thread two(compute, f2, window);
+  sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(500, 500), "My window");
   window->clear();
   compute(f3,window);
   compute(f2,window);
